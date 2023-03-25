7352920164983aef2cdc497fd7cfad30f72b8780
Jan-Luca Klees <email@janlucaklees.de>
2023-03-25 20:39:10 +0100

    Big content update
    - Finalized website structure
    - added legally required pages
    - threw out all unnecessary code and dependencies.
    - included google fonts, to avaoid cookies and gdpr stuff.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,10 +3,6 @@
 
 jQuery(document).ready(function ($) {
 
-    jQuery(window).load(function () {
-        jQuery(".loaded").fadeOut();
-        jQuery(".preloader").delay(1000).fadeOut("slow");
-    });
     /*---------------------------------------------*
      * Mobile menu
      ---------------------------------------------*/
@@ -19,99 +15,15 @@ jQuery(document).ready(function ($) {
     $('ul.navbar-nav li').hover(function () {
         $(this).children('ul').stop(true, false, true).fadeToggle(300);
     });
+
     /*---------------------------------------------*
      * STICKY scroll
      ---------------------------------------------*/
-
     $("").localScroll();
 
     /*---------------------------------------------*
-     * WOW
+     * Main Menu Scroll
      ---------------------------------------------*/
-
-    var wow = new WOW({
-        mobile: false // trigger animations on mobile devices (default is true)
-    });
-    wow.init();
-
-
-    /*---------------------------------------------*
-     * Counter 
-     ---------------------------------------------*/
-
-    $('.statistic-counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-
-    /* ---------------------------------------------------------------------
-     Carousel
-     ---------------------------------------------------------------------= */
-
-    $('.main_home_slider').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: false,
-        navText: [
-            "<i class='lnr lnr-chevron-left'></i>",
-            "<i class='lnr lnr-chevron-right'></i>"
-        ],
-        autoplayHoverPause: true
-
-    });
-
-    $('.main_team').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: true,
-        navText: [
-            "<i class='lnr lnr-chevron-left'></i>",
-            "<i class='lnr lnr-chevron-right'></i>"
-        ],
-        autoplayHoverPause: true
-
-    });
-
-    $('.main_work1_content').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: true,
-        navText: [
-            "prew",
-            "next"
-        ],
-        autoplayHoverPause: true
-
-    });
-    $('.main_work1_team').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: false,
-        navText: [
-            "prew",
-            "next"
-        ],
-        autoplayHoverPause: true
-
-    });
-
-
-// main-menu-scroll
-
     jQuery(window).scroll(function () {
         var top = jQuery(document).scrollTop();
         var height = 5;
@@ -123,28 +35,6 @@ jQuery(document).ready(function ($) {
             jQuery('.navbar-fixed-top').removeClass('menu-scroll');
         }
     });
-
-// scroll Up
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 600) {
-            $('.scrollup').fadeIn('slow');
-        } else {
-            $('.scrollup').fadeOut('slow');
-        }
-    });
-
-    $('.scrollup').click(function () {
-        $("html, body").animate({scrollTop: 0}, 1000);
-        return false;
-    });
-
-    $('.main_mix_content').imagesLoaded(function () {
-        // images have loaded
-    });
-
-
-    //End
 });
 
 
